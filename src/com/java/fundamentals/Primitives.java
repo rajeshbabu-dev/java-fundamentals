5050e0c15bf234570daa2eef1ba76658a3fe68ba
Rajesh babu <useless6859@gmail.com>
2026-02-18 16:43:42 +0530

    chore: primitives

---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,56 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    static void main(String[] args) {
+        // variables declaration
+        // byte
+        byte age;
+        age = 30;
+        System.out.println("age : " + age);
+        byte month;
+        month = 12;
+        System.out.println("month : " + month);
+        byte subjctMarks;
+        subjctMarks = 99;
+        System.out.println("subjectMarks : " + subjctMarks);
+        byte discount;
+        discount = 70;
+        System.out.println("discount : " + discount);
+
+
+        //short
+        short totalMarks;
+        totalMarks = 780;
+        System.out.println("totalMarks : " + totalMarks);
+        short currentYear;
+        currentYear = 2026;
+        System.out.println("currentYear : " + currentYear);
+        short priceOfIngredients;
+        priceOfIngredients = 470;
+        System.out.println("priceOfIngredients : " + priceOfIngredients);
+
+
+        //int
+        int salary;
+        salary = 24356;
+        System.out.println("salary : " + salary);
+        int upiPin;
+        upiPin = 147678;
+        System.out.println("upiPin : " + upiPin);
+        int numberOfUsers;
+        numberOfUsers = 78684885;
+        System.out.println("numberOfUsers : " + numberOfUsers);
+        int secondsPerDay;
+        secondsPerDay = 86100;
+        System.out.println("secondsPerDay : " + secondsPerDay);
+
+
+
+
+
+        //long
+        long mobileNumber;
+        mobileNumber = 6309782995L;
+        System.out.println("mobileNumber : " + mobileNumber);
+    }
+}
